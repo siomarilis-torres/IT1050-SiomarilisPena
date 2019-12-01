16a85940a52f7a2bed92cdd8baf4584c34abca3a
Siomarilis Pena <S00905889@acad.tri-c.edu>
2019-12-01 16:19:58 -0500

    Created Lab 4

---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
+//The four basic elements of the counter controlled repetition require the following: 
+//a loop continuation condition
+//a loop counter
+//an increment or decrement
+//initial value of the controlled variable
+
+// while and for statements execute a statement as long as it remains true. Both statements execute their bodies
+// the for repetition specifies the counter controlled repetition details, wheras in a while statement  its initialized before the loop
+// while and for can be used for either repetition type
+
+    using System;
+
+public class DoWhileTest
+{
+    public static void Main(string[] args)
+    {
+        int counter = 1;// initialize counter
+
+        do
+        {
+            Console.Write("{0}    ", counter);
+            ++counter;
+        } while (counter <= 10);// end do...while
+                                //a while is used when you dont know how many times a loop will execute at runtime. A do while statement is used when the loop executes at least one time
+
+        {
+            private class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                Console.WriteLine(i);
+                {
+                    if ((i % 2) == 0)
+                    { Console.WriteLine("Number is even"); }
+                    else if ((i % 2)! = 0)
+                    {
+                        Console.WriteLine("Number is odd");
+                    }
+                }
+            }
+                private class Program
+        {
+            private static void Main(string[] args)
+            {
+                Console.Write("Please enter a temperature: ");
+
+                int temp = Convert.ToInt32(Console.ReadLine());
+
+                if (temp >= 90) { Console.WriteLine("fish"); }
+
+                else if (temp >= 80) { Console.WriteLine("Lion"); }
+
+                else if (temp >= 70) { Console.WriteLine("Turle"); }
+
+                else if (temp >= 60) { Console.WriteLine("Deer"); }
+
+                else if (temp >= 50) { Console.WriteLine("Reindeer"); }
+
+                else if (temp >= 40) { Console.WriteLine("Moose"); }
+
+                else if (temp >= 20) { Console.WriteLine("Penguin"); }
+
+                else if (temp >= 10) { Console.WriteLine("Polar Bear"); }
+
+                else { Console.WriteLine("Bug"); } }
+        }
+    }
+                for (int i = 10; while i< 21;
+                {
+                Console.WriteLine(i);
+                }
+
+
+                for (int i = 0; i< 101; i++) 
+                { 
+                Console.WriteLine(i); 
+                Console.WriteLine( "********" );
+                }
