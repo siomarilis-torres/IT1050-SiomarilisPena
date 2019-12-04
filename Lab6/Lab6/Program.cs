4e3e0e302e3d49f94a7acf93ccd9a08a29f78240
Siomarilis Pena <S00905889@acad.tri-c.edu>
2019-12-03 21:06:25 -0500

    Created Midterm

--- Lab6/Lab6/Program.cs
+++ Lab6/Lab6/Program.cs
@@ -13,3 +13,128 @@ namespace Lab6
         }
     }
 }
+
+2.	Fill in the blanks in the following statement:
+A one-dimensional array p contains four elements.The array access expressions to access each of the elements in p are __p[0] _, _p[1] _, __p[2] __ and __p[3]____.
+
+3.	Create a 12-element array called months.  Set each element to the name of each month.
+For example months[0] = “January”.  Use a for loop to display the number and name of each month.
+
+    static void Main(string[] args)
+{
+    int[] array = new int[12];
+    month[0] = "january";
+    month[1] = "february";
+    month[2] = "march";
+    month[3] = "april";
+    month[4] = "may";
+    month[5] = "june";
+    month[6] = "july";
+    month[7] = "august";
+    month[8] = "september";
+    month[9] = "october";
+    month[10] = "november";
+    month[11] = "december";
+    {
+        for int counter = 0; counter < array.Length; ++counter):
+        Console.WriteLine(" {0}{1,15}", counter, string[month]);
+    }
+
+    4.Create a 4 - element array to store the names of 4 seasons.Use a foreach loop to display the name of each season.
+
+      {
+        class Program
+{
+    string void Main(string[] args)
+    {
+        string[] seasons = { "Winter", "Spring", "Summer", "Autumn" };
+        Console.WriteLine("The Four Seasons");
+        foreach (string season in seasons)
+        {
+            Console.WriteLine(season);
+
+            5.Create an array of integers with 1000 elements.Fill the array with random numbers.  Use a foreach loop to print all integers in the array.
+        Random random = new Random();
+            int randomNumber
+            randomNumber = random.Next(0, 100);  // place this line in the loop
+
+            {
+                class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array;
+            array = new int[1000];
+            {
+                Random randomNumbers = new Random(1000);
+                int randomNumber = randomNumber.Next();
+                {
+                    foreach (randomNumber in randomNumber.Next(0, 100)) ;  // place this line in the loop
+                    Console.WriteLine("{0}");
+                }
+            }
+        }
+
+        6.	Paste the following code into the main() method.Modify the code; so that it works(You’ll have to make 2 small modifications).  It should output the value of each string in the array called names.
+
+
+string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+
+        int i = 0;
+while (i<names.Length)
+{
+    Console.WriteLine(names[0]);
+}
+    {
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+            int i = 0;
+            {
+                while (i < names.Length)
+                    Console.WriteLine(string[names]);
+            }
+        }
+    }
+
+
+
+  7.	Modify the code from problem 5, so that it outputs a number and then a person’s name using the following output statement.
+
+Console.WriteLine("{0,2}. {1}", i, names[i]);
+
+            
+            class Program
+{
+    static void Main(string[] args)
+    {
+        string[] names = new string[5] { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+        int i = 0;
+        {
+            for (int i = 0; i < names.Length; ++i) ;
+
+            Console.WriteLine("{0,2}. {1}", i, names[i]);
+        }
+    }
+}
+
+
+
+8.	Modify the code from problem 6, so that it uses a foreach loop as opposed to a while loop.The output should appear exactly the same as it did after step 2.
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string[] names = new string[5] { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+        int i = 0;
+        {
+            foreach (string name in names)
+                Console.WriteLine("{0,2}.{1}", i, names[i]);
+        }
+    }
+}
---
+++ Midterm-/Midterm-/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+2.	Identify and briefly describe 3 types of control structures that we can use to order the statements in our programs?  
+
+    if statements:used to evaluate a boolean expression
+    loops:used for interative purposes
+    switch statement: can replace if statements
+
+
+3.	Create an infinite while loop.Use a Boolean variable called keepLooping that set to true in the loop’s termination condition.Hint: Use CTRL+C or Debug -> Terminate All to end the program.
+
+    int i = 12;
+Boolean keepLooping = true;
+    while (keepLooping)
+    {
+        if  (i<= 11
+             keepLooping = false;
+       i++
+       Console.WriteLine(i);
+    }
+}
+
+
+
+
+4.	Write a while loop to prints 2 through 128 in brackets, each on a new line.You should initialize your loop control variable to 2.  Output the value of the loop control variable each time through the loop.Use a condition that ends the loop after 64 is printed.
+
+
+[2]
+[4]
+[8]
+[16]
+[32]
+[64]
+[128]
+
+        class Program
+{
+    static void Main(string{ }
+    args)
+        {           
+            int loop = 2;
+
+            while (loop< 128)
+
+            {   
+                Console.WriteLine("[" + loop + "]")
+
+                loop = loop + loop;
+
+               }
+        }
+}
+
+
+5.	Write a for loop that prints 49 through 1 separated by a comma.Note, you will need to use a condition inside of the loops so it does not print the comma the last time through (no newlines).
+
+49, 48, 47, …, 3, 2, 1
+
+                   string j = ","
+                   int i;
+                   for (i = 49; i >= 1; --1
+                   {
+                        Console.Write(i);
+                        if  (i>=2)
+                        {
+                        Console.Write(j);
+                     }
+                }
+
+6.	Write a while loop that prints all odd numbers 1 through 21 separated by spaces(no newlines).
+
+1 3 5 7 9 11 13 15 17 19 21
+
+                    int i = 1;
+                    while  (i <=21
+                    {
+                        {
+                            if  ((i % 2) ==0)
+                            {
+                                Console.Write(" ");
+                            }
+                            else if  ((i % 2) !=0)
+                            {
+                                Console.Write(i);
+                            }
+                            i++;
+                        }
+                    }
+                    
+
+7.	What is the output for the following code?
+int n = 8;
+int i = 10; // initialize
+do {
+   Console.Write("*");
+   i++; // update!
+} while (i<n); // test condition
+
+What would the output be with the exact same code but using a while statement as opposed to a do-while?
+
+
+    int n = 8;
+int n = 10;
+    while  (i<n)
+    {
+        Console.Write("*");
+            i++
+       }
+
+
+
+8.	Explain how do we combine multiple Boolean values? Write an if statement that outputs “Let’s go outside!” when both Boolean values are false.
+boolean icyRain;
+boolean tornadoWarning;
+
+Boolean values are combined using integers
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+        bool icyrain = false, tornadowarning = false;
+        if icyrain == false && tornadowarning == false)
+        {
+            Console.WriteLine("Lets go outside");
+        }
+    }
+        
+
+
+
+9.	Extra Credit: Use nested loops to print the following to the console:
+
+        123454321
+         1234321
+          12321
+           121
+            1
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int numberoflayer = 5, Space, Number;
+            Console.WriteLine("Print pyramid");
+            for (int i = 1; i <= numberoflayer; i++)
+            {
+                for (Space = 1; Space <= (numberoflayer - i); Space++)
+                    Console.Write("");
+                for (Number = 1; Number <= i; Number++)
+                    Console.Write(Number);
+                for (Number = (i - 1); Number >= 1; Number--)
+                    Console.Write(Number);
+                Console.WriteLine();
+            }
+        }
+
+
